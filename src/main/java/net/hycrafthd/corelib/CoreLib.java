4fd692e9b1414c496b2c95ff03083c9496b90fd9
HyCraftHD <hycrafthd@live.de>
2016-07-12 23:49:53 +0200

    Fixed version update

--- src/main/java/net/hycrafthd/corelib/CoreLib.java
+++ src/main/java/net/hycrafthd/corelib/CoreLib.java
@@ -37,7 +37,7 @@ public class CoreLib extends DummyModContainer {
 	/**
 	 * Allowed Minecraft versions
 	 */
-	public static final String mcversion = "[1.9]";
+	public static final String mcversion = "[1.9.4]";
 	/**
 	 * Current version of CoreLib
 	 */
